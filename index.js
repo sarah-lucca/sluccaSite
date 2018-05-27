3da368c75a58280beb0b7465c8308d3d4626417c
Sarah Lucca <sdlucca1997@gmail.com>
2018-05-28 09:36:25 +1200

    transitions on the bagel menu

---
+++ index.js
@@ -0,0 +1,73 @@
+
+var plainBagel = document.getElementById("plain_bagel");
+var raisinBagel = document.getElementById("raisin_bagel");
+var everythingBagel = document.getElementById("everything_bagel");
+var menulist1 = document.getElementById("menu-list-1");
+var menulist2 = document.getElementById("menu-list-2");
+var menulist3 = document.getElementById("menu-list-3");
+
+// plainBagel.onmouseover = function() {
+// 	var bar = document.getElementById("menu-list-2");
+// 	var text = bar.getElementsByTagName("p")[0];
+//     var id = setInterval(frame, 5);
+//     var barLength = 0;
+//     bar.style.backgroundColor = "white";
+//     if (bar.style.width.localeCompare("46%") == 0) {
+//     	return;
+//     }
+//     function frame() {
+//     	if (barLength == 45) {
+//     		text.style.display = "block";
+//     		clearInterval(id);
+//     	}
+//             barLength++; 
+//             bar.style.width = barLength.toString() + "%";      
+//     }
+//     return false;
+// }
+// menulist2.onmouseleave = function() {
+// 	var bar = document.getElementById("menu-list-2");
+// 	var text = bar.getElementsByTagName("p")[0];
+// 	text.style.display = "none";
+//     var id = setInterval(frame, 5);
+//     var barLength = 45;
+//     function frame() {
+//     	if (barLength == 0) {
+//     		bar.style.backgroundColor = "";
+//     		clearInterval(id);
+//     	}
+//             barLength--; 
+//             bar.style.width = barLength.toString() + "%";      
+//     }
+//     return false;
+// // }
+// raisinBagel.onmouseover = function() {
+// 	var bar = document.getElementById("menu-list-1");
+// 	var text = bar.getElementsByTagName("p")[0];
+//     var id = setInterval(frame, 8);
+//     var barLength = 0;
+//     bar.style.backgroundColor = "white";
+//     function frame() {
+//     	if (barLength == 45) {
+//     		text.style.display = "block";
+//     		clearInterval(id);
+//     	}
+//             barLength++; 
+//             bar.style.width = barLength.toString() + "%";      
+//     }
+// }
+// everythingBagel.onmouseover = function() {
+// 	var bar = document.getElementById("menu-list-3");
+// 	var text = bar.getElementsByTagName("p")[0];
+//     var id = setInterval(frame, 8);
+//     var barLength = 0;
+//     bar.style.backgroundColor = "white";
+//     function frame() {
+//     	if (barLength == 45) {
+//     		text.style.display = "block";
+//     		clearInterval(id);
+//     	}
+//             barLength++; 
+//             bar.style.width = barLength.toString() + "%";      
+//     }
+// }
